62aa37e8e71f66edf0191b24bdde1d38ce3cb9d2
yankyalvarezm <alvarezyanky7@gmail.com>
2024-07-30 17:04:03 -0400

    added logos to the pdf

--- src/Pages/FormGenerator.jsx
+++ src/Pages/FormGenerator.jsx
@@ -2,6 +2,17 @@ import React, { useState, useContext } from "react";
 import "../styles/form-generator.css";
 import { UserContext } from "../context/user.context";
 import { jsPDF } from "jspdf";
+import lachLogo from "../assets/lach-logo.png";
+import spotMeUpLogo from "../assets/spotmeup-logo.png";
+import bunkerLogo from "../assets/bunker-logo.png";
+import neutroLogo from "../assets/neutro-logo.png";
+
+const logos = {
+  Lach: lachLogo,
+  SpotMeUp: spotMeUpLogo,
+  Neutro: neutroLogo,
+  Bunker: bunkerLogo,
+};
 
 const FormGenerator = () => {
   const [letterData, setLetterData] = useState({
@@ -13,6 +24,7 @@ const FormGenerator = () => {
     gasto: "",
     costo: "",
     beneficios: "",
+    tipoEvento: "",
   });
 
   const handleChange = (e) => {
@@ -23,9 +35,21 @@ const FormGenerator = () => {
     }));
   };
 
-  const handleSubmit = (e) => {
+  const loadImage = (imgSrc) => {
+    return new Promise((resolve) => {
+      const img = new Image();
+      img.src = imgSrc;
+      img.onload = () => resolve(img);
+    });
+  };
+
+  const addImageToPDF = (doc, img, x, y, width = 25, height = 25) => {
+    doc.addImage(img, "PNG", x, y, width, height);
+  };
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    generatePDF(letterData);
+    await generatePDF(letterData);
   };
 
   const formatDate = (dateString) => {
@@ -52,7 +76,33 @@ const FormGenerator = () => {
     return `${day} de ${month}, ${year}`;
   };
 
-  const generatePDF = (data) => {
+  const formatDateToday = () => {
+    const months = [
+      "Enero",
+      "Febrero",
+      "Marzo",
+      "Abril",
+      "Mayo",
+      "Junio",
+      "Julio",
+      "Agosto",
+      "Septiembre",
+      "Octubre",
+      "Noviembre",
+      "Diciembre",
+    ];
+
+    const date = new Date();
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+
+    return `${day} de ${month}, ${year}`;
+  };
+
+  const todayFormatted = formatDateToday();
+
+  const generatePDF = async (data) => {
     const doc = new jsPDF();
 
     const pageWidth = doc.internal.pageSize.getWidth();
@@ -63,11 +113,18 @@ const FormGenerator = () => {
       doc.text(text, x, y);
     };
 
+    const rightAlignText = (doc, text, y, pageWidth, marginRight = 10) => {
+      const textWidth = doc.getTextWidth(text);
+      const x = pageWidth - textWidth - marginRight;
+      doc.text(text, x, y);
+    };
+
     const formattedDate = formatDate(data.fechaEvento);
+
     // ---------------
     doc.setFontSize(12);
-    doc.text(`${formattedDate}`, 165, 10);
-    doc.text(`${data.ciudad}, Rep. Dom.`, 153, 16);
+    rightAlignText(doc, `${todayFormatted}`, 10, pageWidth);
+    rightAlignText(doc, `${data.ciudad}, Rep. Dom.`, 16, pageWidth);
     // ---------------
     doc.text(`Señor (es):`, 10, 20);
     doc.text(`${data.dirigida}`, 10, 25);
@@ -83,104 +140,43 @@ const FormGenerator = () => {
     // ---------------
     doc.setFontSize(12);
     doc.setFont("helvetica", "normal");
-    doc.text(`¿Quiénes somos?`, 10, 45);
-
-    // ---------------
     doc.text(
-      `Somos un consorcio de empresas, entre ellas, bares, promotora de eventos y plataforma de boletería ligados a la Holding de radiodifusión más importante de la región norte de nuestro país. En lo que respecta a nuestra negociación, es fundamental el entendimiento de dos de ellas:`,
+      `Queridos destinatarios, es un placer saludarles cordialmente. Nos dirigimos a ustedes con el propósito de expresar nuestro interés en contar con el respaldo de su respetada empresa.`,
       10,
-      55,
+      45,
       { maxWidth: 190 }
     );
 
     // ---------------
 
-    doc.text(
-      `Lach Global: Es una productora de eventos oriunda de nuestra ciudad natal, ${data.ciudad}. Nuestro primer evento fue realizado el pasado sábado ocho (8) de junio en el Salón de Eventos de Shia Restaurant, ${data.ciudad}, en el cual alcanzamos la capacidad máxima de visitantes para el establecimiento (250 personas), con diez (10) días de promoción, en un salón de eventos adecuado por nosotros y únicamente la participación de dj's como atracciones. Nuestro segundo evento fue realizado el pasado sábado veintidós (22) de julio en el Bunker Headquarters, ${data.ciudad}, bajo la modalidad de Open Bar, en cuyo establecimiento agotamos su capacidad máxima (150 personas) con cinco (5) días de promoción y únicamente la participación de dj's como atracciones.`,
-      10,
-      150,
-      { maxWidth: 190 }
-    );
+    // ---------------
 
-    doc.text(
-      `Spot me Up: Plataforma de boletería más innovadora en la República Dominicana, totalmente digital y con amenidades nunca utilizadas en nuestro país y gran parte del caribe. Aún no se ha lanzado al mercado, sigue en pruebas de eventos internos a través de nuestros bares, pero la idea es que sea anunciada y promocionada con los eventos que corresponden a nuestra negociación, los espectáculos de ${data.nombreEvento}.`,
-      10,
-      190,
-      { maxWidth: 190 }
-    );
+    if (data.departede === "Lach & SpotMeUp") {
+      const lachImg = await loadImage(logos["Lach"]);
+      addImageToPDF(doc, lachImg, 10, 225, 50, 25);
 
-    doc.text(`¿Qué queremos?`, 10, 220);
-    doc.text(
-      `Nuestra intención es lograr una colaboración en los eventos planificados para los días viernes diecinueve (19) y veinte (20) de julio del presente año. Los eventos recaen en la participación de la artista de origen Puerto Riqueño 'De La Rose' y un aglomerado de dj's de nacionalidad dominicana, especialmente de La Vega, Santiago De Los Caballeros y Santo Domingo. Dichos eventos, se realizaran de la siguiente manera:`,
-      10,
-      230,
-      { maxWidth: 190 }
-    );
+      const spotMeUpImg = await loadImage(logos["SpotMeUp"]);
+      addImageToPDF(doc, spotMeUpImg, 170, 230, 20, 20);
+    } else {
+      const selectedLogo = logos[data.departede];
+      if (selectedLogo) {
+        const img = await loadImage(selectedLogo);
+        if (data.departede === "Lach") {
+          addImageToPDF(doc, img, 10, 225, 50, 25);
+        } else if (data.departede === "SpotMeUp") {
+          addImageToPDF(doc, img, 10, 230, 20, 20);
+        } else if (data.departede === "Bunker") {
+          addImageToPDF(doc, img, 10, 230, 30, 15);
+        } else if (data.departede === "Neutro") {
+          addImageToPDF(doc, img, 10, 230, 50, 15);
+        } else {
+          addImageToPDF(doc, img, 10, 10, 25, 25);
+        }
+      }
+    }
 
-    doc.text(
-      `Viernes 19 de Julio (La Ciguapa, Santiago De Los Caballeros) El primer evento se realizará en la discoteca que en la actualidad posee el mayor prestigio de la región norte del país, La Ciguapa, la cual esta capacitada para hasta 900 personas. El espectáculo se realizará bajo la modalidad de venta de boletas y reservas de mesas (actividad común de la discoteca).`,
-      10,
-      260,
-      { maxWidth: 190 }
-    );
-
-    doc.text(
-      `Sábado 20 de Julio (Teatro Bisou, Santo Domingo) El segundo evento se realizará en un establecimiento alquilado por nosotros capacitado para hasta 350 personas. Su modalidad será desarrollada bajo la temática de Open Bar y la reserva de mesas, para facilitar al consumidor de optar por la opción de un ticket con bebida incluida o la reserva de un espacio para consumir botellas de la bebida de su preferencia.`,
-      10,
-      280,
-      { maxWidth: 190 }
-    );
-
-    doc.text(`¿Qué Ofrecemos?`, 10, 310);
-    doc.text(
-      `En vista de que los eventos están ideados para todo publico mayor de dieciocho (18) años, ofrecemos una alta capacidad de presencia para sus marcas de manera tradicional y digital, entre ellas, a manera de sugerencia y en capacidad de escuchar propuestas, ofrecemos:`,
-      10,
-      320,
-      { maxWidth: 190 }
-    );
-
-    doc.text(
-      `Publicidad Digital: Redes Sociales (Flyers y Video de Recap)`,
-      10,
-      330,
-      { maxWidth: 190 }
-    );
-    doc.text(
-      `Acceso al marcaje promocional: Stands, Truss y letreros`,
-      10,
-      340,
-      { maxWidth: 190 }
-    );
-    doc.text(`Exclusividad: En la bebida de su tipo`, 10, 350, {
-      maxWidth: 190,
-    });
-    doc.text(
-      `Posible Publicidad a través de medios de radio: @Grupomedrano / @Ritmodelamanana Ponderado para un patrocinador oficial`,
-      10,
-      360,
-      { maxWidth: 190 }
-    );
-
-    doc.text(
-      `En conclusión, nuestro intereses recae en iniciar una colaboración comercial con ustedes a través de estos eventos pautados para los días diecinueve (19) y veinte (20) de julio en Santiago De Los Caballeros y Santo Domingo.`,
-      10,
-      380,
-      { maxWidth: 190 }
-    );
-
-    doc.text(
-      `Sin más por el momento, quedando a la espera de una respuesta afectiva, me despido.`,
-      10,
-      400
-    );
-
-    doc.text(`Att,`, 10, 420);
-    doc.text(`Jean Álvarez Medrano`, 10, 430);
-
-    doc.save("carta-patrocinio.pdf");
+    doc.save(`carta-patrocinio-${data.nombreEvento}.pdf`);
   };
-
-  const { setGenerator } = useContext(UserContext);
 
   return (
     <div className="form-generator-parent">
@@ -202,6 +198,7 @@ const FormGenerator = () => {
               <option value="Lach">Lach</option>
               <option value="SpotMeUp">SpotMeUp</option>
               <option value="Neutro">Neutro</option>
+              <option value="Bunker">Bunker</option>
             </select>
 
             <select
@@ -271,6 +268,7 @@ const FormGenerator = () => {
               onChange={handleChange}
             />
           </div>
+
           <input
             type="text"
             placeholder="Beneficios a ofrecer."
